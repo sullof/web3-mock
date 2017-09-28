b783e2909d107805c73472d896e2f72736db8a1b
James <james@smoothbook.co>
2017-09-28 11:10:47 +0700

    Connect to web3 ok

--- test/web3-mock-test.js
+++ test/web3-mock-test.js
@@ -1,7 +1,8 @@
 
 var chai = require('chai'),
     expect = chai.expect,
-    Web3 = require('../lib/web3mock');
+    Web3 = require('../lib/web3mock'),
+    web3Test = null;
 
 describe('Web3 mock module',
     function(){
@@ -26,8 +27,40 @@ describe('Web3 mock module',
                 expect(Web3.providers).to.not.be.undefined;
 
             }
-        )
+        );
 
+        it('Should require an argument to the HttpProvider constructor',
+            function(){
+
+                expect(
+                    function(){
+
+                        Web3.providers.HttpProvider();
+
+                    }
+                ).to.throw();
+
+            }
+        );
+
+        it('Should connect to a web3 provider',
+            function(){
+
+                var nodeURI = 'http://localhost:8645';
+                web3Test = new Web3(new Web3.providers.HttpProvider(nodeURI));
+
+                expect(typeof web3Test.isConnected).to.equal('function');
+
+            }
+        );
+
+        it('Should return whether it is connected to a blockchain',
+            function(){
+
+                expect(web3Test.isConnected()).to.equal(true);
+
+            }
+        )
 
 
 
