ab500baf2aefee6cb4111b27c5a50514d943affb
James <james@smoothbook.co>
2017-09-28 10:17:54 +0700

    Install mocha + chai

---
+++ test/web3-mock-test.js
@@ -0,0 +1,19 @@
+
+var chai = require('chai'),
+    expect = chai.expect;
+
+describe('Web3 mock module',
+    function(){
+
+        it('Should be true',
+            function(){
+
+                expect(true).to.equal(true);
+
+            }
+        )
+
+
+
+    }
+);
